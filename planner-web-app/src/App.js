8eb0d05802485aa897b59f26b5169592d403a25b
crsee <corey_see@sfu.ca>
2020-11-06 18:21:24 -0800

    fixed update to calendar

--- planner-web-app/src/App.js
+++ planner-web-app/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import EventCalendar from './component/EventCalendar';
 import EventForm from './component/EventForm';
 
 
@@ -9,15 +8,8 @@ function App() {
 
 
   return (
-      <div className ="container">
-        <div className="col1">
-          <h1>FORM</h1>
           <EventForm/>
-        </div>
-        <div className="col2">
-          <EventCalendar/>
-        </div>
-      </div>
+
   );
 }
 
--- planner-web-app/src/component/EventForm.js
+++ planner-web-app/src/component/EventForm.js
@@ -1,13 +1,48 @@
 import React from "react";
 import axios from "axios";
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import timeGridPlugin from "@fullcalendar/timegrid";
+import interactionPlugin from "@fullcalendar/interaction";
 
 export default class MyForm extends React.Component{
 
-    state = {  name: '',
-                description: '',
-                start_date: '',
-                end_date:''}
+    state = {dataBaseEvents:[],
+        dataBaseEventsFormatted:[],
+        name: '',
+        description: '',
+        start_date: '',
+        end_date:''}
 
+    componentDidMount= () => {
+        this.getEventData();
+    }
+
+    getEventData = ()=>{
+        axios.get('/api')
+            .then((response)=>{
+                const data = response.data;
+                this.setState({dataBaseEvents:data});
+                console.log('Data has been populated');
+                console.log(this.state.dataBaseEvents);
+                //this.state.dataBaseEvents.forEach(this.formatEventData(entry));
+                this.formatEventData();
+            })
+            .catch(()=>{
+                alert('Error retrieving data')
+            })
+
+    }
+
+    formatEventData(){
+        const list = [];
+        this.state.dataBaseEvents.forEach(function(entry){
+            list.push({title: entry.name, "start":entry.start_date, "end":entry.end_date})
+            console.log(entry);
+        });
+        this.setState({dataBaseEventsFormatted : list});
+        console.log(this.state.dataBaseEventsFormatted);
+    }
 
     handleChange=({target}) =>{
         const {name, value} = target;
@@ -43,6 +78,7 @@ export default class MyForm extends React.Component{
             .then(() =>{
                 console.log("data has been sent to the server");
                 this.resetUserInputs();
+                this.getEventData();
         })
             .catch((error) =>{
                 console.log(error);
@@ -65,42 +101,74 @@ export default class MyForm extends React.Component{
     render(){
         console.log('state ', this.state)
         return(
-            <form onSubmit={this.handleSubmit}>
-            <label>
-                Name of Event:
-                <input type="text"
-                       name="name"
-                       value ={this.state.name}
-                       onChange={this.handleChange}/>
-            </label>
-            <br></br>
-            <label>
-                Description of event:
-                <textarea type="text"
-                          name="description"
-                          cols="30" rpws="20"
-                          value={this.state.description}
-                          onChange={this.handleChange}/>
-            </label>
-            <br></br>
-            <label>
-                Start date:
-                <input type="date"
-                       name="start_date"
-                       value={this.state.start_date}
-                       onChange={this.handleChange}/>
-            </label>
-            <br></br>
-            <label>
-                End date:
-                <input type="date"
-                       name="end_date"
-                       value={this.state.end_date}
-                       onChange={this.handleChange}/>
-            </label>
-            <br></br>
-            <input type="submit" value="Submit" />
-        </form>
+            <div className="container">
+                <div className="col1">
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name of Event:
+                            <input type="text"
+                                   name="name"
+                                   value ={this.state.name}
+                                   onChange={this.handleChange}/>
+                        </label>
+                        <br></br>
+                        <label>
+                            Description of event:
+                            <textarea type="text"
+                                      name="description"
+                                      cols="30" rpws="20"
+                                      value={this.state.description}
+                                      onChange={this.handleChange}/>
+                        </label>
+                        <br></br>
+                        <label>
+                            Start date:
+                            <input type="date"
+                                   name="start_date"
+                                   value={this.state.start_date}
+                                   onChange={this.handleChange}/>
+                        </label>
+                        <br></br>
+                        <label>
+                            End date:
+                            <input type="date"
+                                   name="end_date"
+                                   value={this.state.end_date}
+                                   onChange={this.handleChange}/>
+                        </label>
+                        <br></br>
+                        <input type="submit" value="Submit" />
+                    </form>
+                </div>
+                <div className="col2">
+                    <FullCalendar
+                        plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
+                        headerToolbar={{
+                            left: 'prev,next today',
+                            center: 'title',
+                            right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                        }}
+                        contentHeight='auto'
+                        initialView='dayGridMonth'
+                        editable={true}
+                        selectable={true}
+                        selectMirror={true}
+                        dayMaxEvents={true}
+                        events={this.state.dataBaseEventsFormatted}
+                        //weekends={this.state.weekendsVisible}
+                        //initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+                        //select={this.handleDateSelect}
+                        //eventContent={renderEventContent} // custom render function
+                        //eventClick={this.handleEventClick}
+                        //eventsSet={this.handleEvents} // called after events are initialized/added/changed/removed
+                        /* you can update a remote database when these fire:
+                        eventAdd={function(){}}
+                        eventChange={function(){}}
+                        eventRemove={function(){}}
+                        */
+                    />
+                </div>
+            </div>
         );
     }
 }
