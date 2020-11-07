7f3c12a2eac1a708bf7e8e68bcfb14701081e36f
kci1 <kci1@sfu.ca>
2020-11-06 21:27:55 -0800

    added basic image upload, DB to be connected later

--- planner-web-app/src/App.js
+++ planner-web-app/src/App.js
@@ -1,26 +1,66 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import ReactDOM from "react-dom";
 
 function App() {
+  const uploadedImage = React.useRef(null);
+  const imageUploader = React.useRef(null);
+
+  const handleImageUpload = e => {
+    const [file] = e.target.files;
+    if (file) {
+      const reader = new FileReader();
+      const { current } = uploadedImage;
+      current.file = file;
+      reader.onload = e => {
+        current.src = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center"
+      }}
+    >
+      <input
+        type="file"
+        accept="image/*"
+        onChange={handleImageUpload}
+        ref={imageUploader}
+        style={{
+          display: "none"
+        }}
+      />
+      <div
+        style={{
+          height: "360px",
+          width: "640px",
+          border: "1px black"
+        }}
+        onClick={() => imageUploader.current.click()}
+      >
+        <img
+          ref={uploadedImage}
+          style={{
+            width: "100%",
+            height: "100%",
+            position: "acsolute"
+          }}
+        />
+      </div>
+      Click to upload Image
     </div>
   );
 }
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
 
 export default App;
