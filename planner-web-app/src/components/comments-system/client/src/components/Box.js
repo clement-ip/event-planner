50278d1402b382c5344b1f873ce6369562c0560f
ewsuen <ewsuen@sfu.ca>
2020-11-07 18:06:27 -0800

    [1] Implemented post and get functionality [2] Component re-renders when a new comment is submitted [3] Comments are synchronized across different clients

--- planner-web-app/src/components/comments-system/client/src/components/Box.js
+++ planner-web-app/src/components/comments-system/client/src/components/Box.js
@@ -1,16 +1,65 @@
 import React, {Component} from 'react';
 import List from './List'
-// import Form from './Form'
-
 import './Box.css';
+import axios from 'axios';
+import io  from "socket.io-client"
+
+const SERVER = "http://localhost:5000";
 
 class Box extends Component {
+
+    constructor(props){
+      super(props);
+      this.state = {
+        comments: []
+      };
+      this.handleSubmit = this.handleSubmit.bind(this);
+      this.fetchData = this.fetchData.bind(this);
+    }
+
+    componentDidMount() {
+      axios.get('/comment')
+        .then(({data}) => this.setState({ comments: data }))
+        .catch(e => console.log(e))
+      const socket = io(SERVER, {transports: ['websocket']});
+      socket.on('Comment', (msg) => {
+        console.log("Received: ", msg);
+        this.fetchData();
+      });
+    }
+
+    fetchData() {
+      axios.get('/comment')
+        .then(({data}) => this.setState({ comments: data }))
+        .catch(e => console.log(e))
+    }
+
+    handleSubmit(e) {
+      e.preventDefault();
+      const newComment = JSON.stringify({name: e.target.name.value, message: e.target.message.value});
+      console.log("New Comment: ", newComment);
+      const socket = io(SERVER, {transports: ['websocket']});
+      socket.emit('Comment', newComment);
+      axios.post('http://localhost:5000/comment', newComment, {
+        headers: {
+            'Content-Type': 'application/json',
+            }
+          }
+        )
+        .then(response => {
+          this.fetchData();
+        })
+    };
+
+    css = `.hide { position:absolute; top:-1px; left:-1px; width:1px; height:1px; }`
     render() {
         return (
           <section className="box">
             <h1>Comments</h1>
-            <List data={this.props.data} />
-            <form method="POST" action="http://localhost:5000/post">
+            <List data={this.state.comments} />
+            <style>{this.css}</style>
+            <iframe title="hiddenFrame" name="hiddenFrame" className="hide"></iframe>
+            <form method="POST" id="form" target="hiddenFrame" onSubmit={this.handleSubmit}>
                 <input type="text" id="name" name="name" placeholder="Name" ></input>
                 <input type="text" id="message" name="message" placeholder="Message"></input><br></br>
               <section className="btn">
--- planner-web-app/src/components/comments-system/index.js
+++ planner-web-app/src/components/comments-system/index.js
@@ -1,24 +1,72 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-const cors = require('cors');
+const Comment = require('./models/comment');
 const port = process.env.PORT || 5000
 
 const app = express();
+const cors = require('cors');
+const comment = require('./models/comment');
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 
 app.use(bodyParser.urlencoded({
     extended: true
  }));
 
-app.use(bodyParser.json());
 app.use(cors());
+app.options('*', cors());
+
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-    res.send({ message: "We did it!" });
+    res.send({ message: "Home" });
   });
-  
-app.post('/post', (req, res) => {
-    res.json({ name: req.body.name, message: req.body.message });
+
+app.get('/comment', cors(), (req, res) => {
+    comment.find({}, function(err,result) {
+      if (err) {
+        res.send(err);
+      }
+      else {
+        res.send(result);
+      }
+    })
+});
+
+app.post('/comment', (req, res) => {
+    const data = req.body;
+    const newComment = new Comment(data);
+    newComment.save((error) => {
+      if (error) {
+        res.status(500).json({msg: 'Error'});
+      }
+      else {
+        res.json({
+          msg: 'Received data: ', data
+        })
+      }
+    })
+});
+
+mongoose.connect("mongodb+srv://470User:CMPT470@470cluster.tajiy.mongodb.net/userdata?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+mongoose.connection.on("error", err => {
+  console.log("err", err)
+})
+mongoose.connection.on("connected", (err, res) => {
+  console.log("Mongoose is connected")
 })
 
-app.listen(port, () => console.log("Backend server live on " + port));
+io.on('connection', (socket) => {
+  console.log('A user has connected');
+  socket.on('Comment', (msg) => {
+    io.emit('Comment', msg);
+  });
+});
+
+http.listen(port, () => {
+  console.log(`Listening on *:${port}`);
+});
