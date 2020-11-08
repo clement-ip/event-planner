a28f92ed929a0af854fe11027dcc09c09aec7822
eclam <lameric1994@gmail.com>
2020-11-07 22:26:29 -0800

    clean up pages

--- planner-web-app/src/pages/index.jsx
+++ planner-web-app/src/pages/index.jsx
@@ -1,14 +1,30 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 
+// import EventForm from './components/calendar/EventForm';
+// import ImageUploader from './components/ImageUploader/ImageUpload.js'
+
+// import NavBar from "./components/navbar/navbar"
+// import CommentBox from "./components/comments-system/client/src/components/Box"
+// import ProfilePage from "./pages/profile_page"
+
 const MainPage = () => {
   return(
     <div>
       <h3>Main Page</h3>
       <p>Test Main Page</p>
-      <p>
+      <div>
         <Link to="/profile">Profile Page</Link>
-      </p>
+      </div>
+      <div>
+        <Link to="/EventForm">Calendar</Link>
+      </div>
+      <div>
+      <Link to="/ImageUploader">Image Upload</Link>
+      </div>
+      <div>
+        <Link to="/CommentBox">Comment Section</Link>
+      </div>
     </div>
   );
 }
