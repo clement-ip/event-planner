2a73fec3c650bdf6300c4cb57c075d3b9d5e3be5
kci1 <kci1@sfu.ca>
2020-11-07 21:04:34 -0800

    modularize ImageUploader

--- planner-web-app/src/App.js
+++ planner-web-app/src/App.js
@@ -1,66 +1,13 @@
 import React from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import ReactDOM from "react-dom";
+import ImageUploader from './components/ImageUploader/ImageUpload.js'
 
 function App() {
-  const uploadedImage = React.useRef(null);
-  const imageUploader = React.useRef(null);
-
-  const handleImageUpload = e => {
-    const [file] = e.target.files;
-    if (file) {
-      const reader = new FileReader();
-      const { current } = uploadedImage;
-      current.file = file;
-      reader.onload = e => {
-        current.src = e.target.result;
-      };
-      reader.readAsDataURL(file);
-    }
-  };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center"
-      }}
-    >
-      <input
-        type="file"
-        accept="image/*"
-        onChange={handleImageUpload}
-        ref={imageUploader}
-        style={{
-          display: "none"
-        }}
-      />
-      <div
-        style={{
-          height: "360px",
-          width: "640px",
-          border: "1px black"
-        }}
-        onClick={() => imageUploader.current.click()}
-      >
-        <img
-          ref={uploadedImage}
-          style={{
-            width: "100%",
-            height: "100%",
-            position: "acsolute"
-          }}
-        />
-      </div>
-      Click to upload Image
-    </div>
+    <ImageUploader/>
   );
 }
-
-const rootElement = document.getElementById("root");
-ReactDOM.render(<App />, rootElement);
 
 export default App;
