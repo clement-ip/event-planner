94e131d72568df7add9dd3d4fb6a29b5f47fc1be
Syed Hassan Ali <shali@sfu.ca>
2020-11-12 20:05:22 -0500

    backend authentication done for register, login, and logout. Front end pages created, need to connect. Please don't merge this branch ever

---
+++ planner-web-app/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    atlasURI: "mongodb+srv://470User:CMPT470@470-external.wozmd.mongodb.net/database",
+    secretOrKey: "secret"
+}
---
+++ planner-web-app/model/User.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    name : {
+        type: String,
+        required: true
+    },
+    email : {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+    },
+    password : {
+        type: String,
+        required: true,
+        minlength: 6
+    },
+});
+
+userSchema.pre('save', function(next){
+    // If password is modified no need to hash
+    // if(this.isModified('password'))
+    //     return next();
+    const saltRounds = 10;
+
+    bcrypt.genSalt(saltRounds, (err, salt) => {
+        bcrypt.hash(this.password, salt, (err, hash) => {
+            if(err)
+                return next(err);
+            this.password = hash;
+            next();
+        });
+    })
+});
+
+userSchema.methods.comparePassword = function(password,cb){
+    bcrypt.compare(password, this.password, (err, isMatch) => {
+        if(err)
+            return cb(err);
+        else
+            if(!isMatch)
+                return cb(null, isMatch);
+            return cb(null, this);
+    })
+}
+
+const User = mongoose.model('user', userSchema);
+
+module.exports = User;
