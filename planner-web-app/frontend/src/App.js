c3b64f21c3df085ed64e7edc42557325b8f78ad8
Syed Hassan Ali <shali@sfu.ca>
2020-11-20 04:33:48 -0500

    renamed client folder

---
+++ planner-web-app/frontend/src/App.js
@@ -0,0 +1,37 @@
+import React, {useContext } from "react";
+import { Route, Redirect } from "react-router-dom";
+
+import { AuthContext } from './Context/AuthContext';
+import Navbar from "./components/Navbar";
+import Landing from "./components/Landing";
+import Hero from "./components/Hero";
+
+
+import AuthService from './Services/AuthService';
+
+function App() {
+  // const person = {
+  //   name: "Hassan",
+  //   email : "shali108@sfu.ca",
+  //   password : "password"
+  // };
+
+  // AuthService.register(user).then(data => console.log(data))
+  // AuthService.login(person).then(data => console.log(data))
+  // AuthService.isAuthenticated().then(data => console.log(data));
+  // AuthService.logout()
+
+
+  const { isAuthenticated, setIsAuthenticated } = useContext(AuthContext);
+
+  return (
+      <div className="App">
+        <Navbar />
+        { isAuthenticated ? <Redirect to="/hero"/>  : null}
+        <Route exact path="/" component={Landing} />
+        <Route exact path="/hero" component={Hero} />
+      </div>
+  );
+}
+
+export default App;
