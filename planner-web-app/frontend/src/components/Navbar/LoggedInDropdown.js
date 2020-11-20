22817b9d0ca11b9a934158f4abf673f5312dd9f8
Syed Hassan Ali <shali@sfu.ca>
2020-11-20 17:32:05 -0500

    added a logged in navbar dropdown with logout and profile buttons. Restructured Navbar.js and subcomponents into their own directory

---
+++ planner-web-app/frontend/src/components/Navbar/LoggedInDropdown.js
@@ -0,0 +1,42 @@
+import React, { useContext } from "react";
+
+import AuthService from '../../Services/AuthService';
+import { AuthContext } from '../../Context/AuthContext';
+
+const LoggedOutDropdown = () => {
+    const { setIsAuthenticated } = useContext(AuthContext);
+
+    const logOutHandler = () => {
+        AuthService.logout();
+        setIsAuthenticated(false);
+    }
+
+    return ( 
+        <div className="navbar-menu">
+        {/* Signup/Login Dropdown */}
+            <div className="navbar-end">
+                <div className="navbar-item is-hoverable">
+                    <button className="button is-rounded is-white mr-4">
+                        <span className="icon">
+                            <i className="fas fa-bars"></i>
+                        </span>
+                        <span className="icon">
+                            <i className="fas fa-user-circle" style={{width: "2em", height: "2em"}}></i>
+                        </span>
+                    </button>
+                    <div className="navbar-dropdown is-right is-boxed mt-1 mr-5" style={{width: "170px"}}>
+                        <a className="dropdown-item">
+                            Profile
+                        </a>
+                        <hr className="navbar-divider"></hr>
+                        <a onClick={logOutHandler} className="dropdown-item">
+                            Log Out
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+     );
+}
+ 
+export default LoggedOutDropdown;
