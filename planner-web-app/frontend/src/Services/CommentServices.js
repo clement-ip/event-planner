9de3f3331878e28a400fbec5a1574d1907978c5c
Syed Hassan Ali <shali@sfu.ca>
2020-11-30 15:19:17 -0500

    modularized the comment front end. Routes are in their own service folder, and are properly error handled. Changed box.js to accomodate changes to the response data.

--- planner-web-app/frontend/src/Services/CommentServices.js
+++ planner-web-app/frontend/src/Services/CommentServices.js
@@ -1,51 +1,39 @@
 export default {
-    login : user => {
-        return fetch('http://localhost:5000/login', {
-            credentials : 'include',
-            method : "post",
-            body : JSON.stringify(user),
-            headers : {
-                'Content-Type' : 'application/json'
-            }
+    commentList : eventID => {
+        return fetch('http://localhost:5000/comment/' + eventID, {
+            credentials : 'include'
         })
             .then(res => {
-                if(res.status !== 401)
-                    return res.json().then(data => data);
+                if(res.status !== 401) {
+                    if (res.status === 500)
+                        return res.json().then(({ message }) => {
+                           return { isAuthenticated : true, message, data : null};
+                        });
+                    else
+                        return res.json().then(({ message, data }) => {
+                            return { isAuthenticated: true, message, data};
+                        });
+                }
                 else
-                    return { isAuthenticated : false, user : { email : "" }};
+                    return { isAuthenticated : false, message : { msgBody : "Not Authenticated", msgError : true}, data : null};
             });
     },
-    register : user => {
-        return fetch('http://localhost:5000/register', {
+    createComment : commentData => {
+        return fetch('http://localhost:5000/comment', {
+            credentials : 'include',
             method : "post",
-            body : JSON.stringify(user),
+            body : JSON.stringify(commentData),
             headers : {
                 'Content-Type' : 'application/json'
             }
         })
-            .then(res => res.json())
-            .then(data => data);
-    },
-    logout : () => {
-        return fetch('http://localhost:5000/logout', {
-            credentials : 'include'
-        })
             .then(res => {
                 if(res.status !== 401)
-                    return res.json().then(data => data);
+                    return res.json().then(({ message }) => {
+                        return { isAuthenticated : true, message };
+                    });
                 else
-                    return { user: { email : "" }, success : false };
-            })
-    },
-    isAuthenticated : () => {
-        return fetch('http://localhost:5000/authenticated', {
-            credentials : 'include'
-        })
-            .then(res => {
-                if(res.status !== 401)
-                    return res.json().then(data => data);
-                else
-                    return { isAuthenticated : false, user : { email : ""}};
+                    return { isAuthenticated : false, message : { msgBody : "Not Authenticated", msgError : true}};
             });
     }
 }
