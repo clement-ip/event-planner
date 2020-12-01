4e7af9f92873f625a37762c9dc7083a2c8a5d86a
kci1 <kci1@sfu.ca>
2020-12-01 02:02:00 -0800

    converted from class to function

--- planner-web-app/frontend/src/components/Carousel/Carousel.js
+++ planner-web-app/frontend/src/components/Carousel/Carousel.js
@@ -1,38 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from 'react';
 import bulmaCarousel from "bulma-carousel/dist/js/bulma-carousel.min.js";
 import "bulma-carousel/src/sass/index.sass";
+
 import CarouselTestData from './CarouselTestData.json';
 
-class Carousel extends React.Component {
-  constructor(props) {
-      super(props)
-      this.state = {
-          CarouselTestData: [],
-      }
-  }
+function Carousel(props) {
+  const [carouselData, setCarouselData] = useState(null);
 
-  componentDidMount() {
+  // let data = props.data["items"]
+  // constructor(props) {
+  //     super(props)
+  //     this.state = {
+  //         CarouselTestData: [],
+  //     }
+  // }
+
+  useEffect(() => {
+    // let path = './CarouselTestData.json';
+    // fetch(path)
+    //   .then(res => res.json())
+    //   .then(carouselData => setCarouselData(carouselData))
+
+
     bulmaCarousel.attach('#carousel-items', {
       slidesToScroll: 1,
       slidesToShow: 4,
       pagination: false,
       infinite: true
     });
-  }
-  render() {
+
+  },[]); 
+
+  // const componentDidMount = () => {
+  //   bulmaCarousel.attach('#carousel-items', {
+  //     slidesToScroll: 1,
+  //     slidesToShow: 4,
+  //     pagination: false,
+  //     infinite: true
+  //   });
+  // }
+
+  // render() {
     return(
-        <section className="section">
-          <div className="carousel-container">
-            <div id="carousel-items" className="carousel">
-              {
-                CarouselTestData.items.map((CarouselItem, index) =>(
+      <section className="section">
+        <div className="carousel-container">
+          <div id="carousel-items" className="carousel">
+            {
+              // carouselData && carouselData.items.map((CarouselItem, index) =>{
+              CarouselTestData.items.map((CarouselItem, index) =>{
+                return (
                   <div className="card" key={index}>
                     <div className="card-image">
                       <figure className="image is-16by9 is-covered">
                         <img
                           src={CarouselItem.image}
-                          alt="" 
-                        />
+                          alt="" />
                       </figure>
                     </div>
                     <div className="card-content">
@@ -44,13 +66,14 @@ class Carousel extends React.Component {
                       </div>
                     </div>
                   </div>
-                ))
-              }
-            </div>
+                );
+              })
+            }
           </div>
-        </section>
+        </div>
+      </section>
     );
-  }
+  // }
 }
 
 export default Carousel;
