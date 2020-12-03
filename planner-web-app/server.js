02ea9ef32d27f4cb7a5dad4460230f6431db1fce
kci1 <kci1@sfu.ca>
2020-12-03 15:23:37 -0800

    image uploads to db, carousel won't read db json

--- planner-web-app/server.js
+++ planner-web-app/server.js
@@ -5,6 +5,12 @@ const cors = require('cors');
 const Event = require('./model/Event');
 const profileRoutes = require('./routes/profilePageRoutes');
 
+const multer = require("multer");
+
+const GridFsStorage = require("multer-gridfs-storage");
+const Grid = require("gridfs-stream");
+const crypto = require("crypto");
+
 const authRoutes = require('./routes/authRoutes');
 const commentRoutes = require('./routes/commentRoutes');
 const eventRoutes = require('./routes/eventRoutes');
@@ -59,3 +65,61 @@ io.on('connection', (socket) => {
 });
 
 
+
+let gfs;
+
+const conn = mongoose.createConnection(atlasURI);
+
+conn.once("open", () => {
+  gfs = Grid(conn.db, mongoose.mongo);
+  gfs.collection("images");
+  console.log("Connection Successful");
+});
+
+// Create storage engine
+const storage = new GridFsStorage({
+  url: atlasURI,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+        const filename = file.originalname;
+        const fileInfo = {
+          filename: filename,
+          bucketName: "images"
+        };
+        resolve(fileInfo);
+      });
+    });
+  }
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("img"), (req, res, err) => {
+  res.send(req.files);
+});
+
+app.get("/upload/:filename", (req, res) => {
+  gfs.files.findOne({ filename: req.params.filename }, (err, file) => {
+    // Check if file
+    if (!file || file.length === 0) {
+      return res.status(404).json({
+        err: "No file exists"
+      });
+    }
+
+    // Check if image
+    if (file.contentType === "image/jpeg" || file.contentType === "image/png") {
+      // Read output to browser
+      const readstream = gfs.createReadStream(file.filename);
+      readstream.pipe(res);
+    } else {
+      res.status(404).json({
+        err: "Not an image"
+      });
+    }
+  });
+});
