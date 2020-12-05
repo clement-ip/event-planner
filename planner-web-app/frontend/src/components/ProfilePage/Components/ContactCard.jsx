8d5aa653e05bd82a01c1d910f82822616398ae9e
eclam <lameric1994@gmail.com>
2020-12-04 23:13:20 -0800

    disable ccard

--- planner-web-app/frontend/src/components/ProfilePage/Components/ContactCard.jsx
+++ planner-web-app/frontend/src/components/ProfilePage/Components/ContactCard.jsx
@@ -4,21 +4,21 @@ const ContactCard = (props) => {
 
     const portfolio_data = props.card_data.portfolio;
 
-    const portfolio_websites = [];
-    for (let i = 0; i < portfolio_data.length; i++) {
-        for (var website in portfolio_data[i]){
-            var website_string = portfolio_data[i][website].toString().toLowerCase();
+    // const portfolio_websites = [];
+    // for (let i = 0; i < portfolio_data.length; i++) {
+    //     for (var website in portfolio_data[i]){
+    //         var website_string = portfolio_data[i][website].toString().toLowerCase();
 
-            if (!website_string.includes("https://") || !website_string.includes("http://")) {
-                website_string = "https://"+ website_string
-            }
-            portfolio_websites.push(
-                    <div>{website}:
-                         <a rel={'external'} target="_blank" href={website_string}> {website_string}</a>
-                    </div>
-            )
-        }
-    }
+    //         if (!website_string.includes("https://") || !website_string.includes("http://")) {
+    //             website_string = "https://"+ website_string
+    //         }
+    //         portfolio_websites.push(
+    //                 <div>{website}:
+    //                      <a rel={'external'} target="_blank" href={website_string}> {website_string}</a>
+    //                 </div>
+    //         )
+    //     }
+    // }
 
 
     return (
