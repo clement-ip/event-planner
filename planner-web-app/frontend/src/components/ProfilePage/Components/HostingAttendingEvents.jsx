4fab4fae48230f2a126fce4f2be0f5290bafc6cd
crsee <corey_see@sfu.ca>
2020-12-06 13:17:34 -0800

    uncomment profile attendingEvents

--- planner-web-app/frontend/src/components/ProfilePage/Components/HostingAttendingEvents.jsx
+++ planner-web-app/frontend/src/components/ProfilePage/Components/HostingAttendingEvents.jsx
@@ -2,12 +2,24 @@ import React from 'react';
 
 const HostingAttendingEvents = (props) => {
 
+    function returnEvents(events){
+        console.log(events.length)
+        var retString = ""
+        for(var x=0; x<events.length;x++){
+            console.log(events[x])
+            retString+="<link>THE EVENT: " +events[x]+ "</link><br/>"
+        }
+        console.log(retString);
+        return retString;
+    }
+
+
     return (
         <div className="Profile_Events_Card">
             <h1><strong>Events</strong>: </h1>
             {/* NOTE: Not working b/c need to populate fields */}
             {/* <p>{props.events_data.hostingEvents}</p> */}
-            {/* <p>{props.events_data.attendingEvents}</p> */}
+             <div>{returnEvents(props.events_data.attendingEvents)}</div>
         </div>
 
     )
