61fa00c68607f6e7d720cddb066db2ade1c695fd
kci1 <kci1@sfu.ca>
2020-12-06 15:21:27 -0800

    upload works, needs refactoring

--- planner-web-app/frontend/src/components/Uploader/ImageUpload.js
+++ planner-web-app/frontend/src/components/Uploader/ImageUpload.js
@@ -16,9 +16,9 @@ function ImageUpload(){
       console.log(r);
     });
 
-    // document
-    //   .getElementById("img")
-    //   .setAttribute("src", `http://localhost:5000/uploads/${file[0].name}`);
+    document
+      .getElementById("img")
+      .setAttribute("src", `http://localhost:3000/${file[0].name}`);
     console.log(file[0]);
   };
 
@@ -58,64 +58,3 @@ function ImageUpload(){
 }
 
 export default ImageUpload;
-
-// class ImageUpload extends React.Component {
-//   Post = e => {
-//     e.preventDefault();
-//     const file = document.getElementById("inputGroupFile01").files;
-//     const formData = new FormData();
-
-//     formData.append("img", file[0]);
-
-//     fetch("/upload", {
-//       method: "POST",
-//       body: formData
-//     }).then(r => {
-//       console.log(r);
-//     });
-
-//     document
-//       .getElementById("img")
-//       .setAttribute("src", `/upload/${file[0].name}`);
-//     console.log(file[0]);
-//   };
-
-//   render() {
-//     return (
-//       <div className="container">
-//         {/* <div className="jumbotron">
-//           <h1 className="display-4">Image Uplaoder</h1>
-//           <p className="lead">
-//             This is a simple application to upload and retrieve images from a
-//             database
-//           </p>
-//           <hr className="my-4" />
-//         </div> */}
-//         <div className="input-group mb-3">
-//           <div className="custom-file">
-//             <input
-//               type="file"
-//               className="custom-file-input"
-//               id="inputGroupFile01"
-//               aria-describedby="inputGroupFileAddon01"
-//             />
-//             <label className="custom-file-label" htmlFor="inputGroupFile01">
-//               Choose file
-//             </label>
-//           </div>
-//         </div>
-//         <button type="button" className="btn btn-primary" onClick={this.Post}>
-//           Upload
-//         </button>
-//         <img
-//           id="img"
-//           style={{
-//             display: "block"
-//           }}
-//         ></img>
-//       </div>
-//     );
-//   }
-// }
-
-// export default ImageUpload;
