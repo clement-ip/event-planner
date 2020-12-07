974000fa7d5cf42b7a707537985c406fcb420a05
kci1 <kci1@sfu.ca>
2020-12-06 22:09:12 -0800

    added delete functionality

--- planner-web-app/server.js
+++ planner-web-app/server.js
@@ -134,3 +134,15 @@ app.get("/image/:filename", (req, res) => {
     }
   });
 });
+
+// @route DELETE /files/:id
+// @desc  Delete file
+app.delete('/files/:id', (req, res) => {
+  gfs.remove({ _id: req.params.id, root: 'uploads' }, (err, gridStore) => {
+    if (err) {
+      return res.status(404).json({ err: err });
+    }
+
+    res.redirect('/');
+  });
+});
