869137be80bfd71e231256e52b18dc09c9124d16
eclam <lameric1994@gmail.com>
2020-12-07 16:41:36 -0800

    updated single event page

--- planner-web-app/frontend/src/components/CreateEvent/CreateEvent.js
+++ planner-web-app/frontend/src/components/CreateEvent/CreateEvent.js
@@ -1,13 +1,8 @@
 import React, {useEffect, useState, useRef } from "react";
-<<<<<<< HEAD
-=======
-import axios from "axios";
-
-import Calendar from '../Calendar/Calendar'
->>>>>>> 6bd81e1... adding panel functionality
 import './EventForm.css';
 import './EventFormBulma.sass';
 import EventForm from "./EventForm";
+
 
 function EventCalendar(){
 
