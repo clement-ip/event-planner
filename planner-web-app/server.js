46658674f6e0c670a572b080d2c682bbb6f405f9
eclam <lameric1994@gmail.com>
2020-12-07 22:08:14 -0800

    fix rebase error

--- planner-web-app/server.js
+++ planner-web-app/server.js
@@ -193,12 +193,3 @@ app.delete("/files/:id", (req, res) => {
       // res.redirect("/")
   })
 })
-var session = require('express-session')
-app.set('trust proxy',1);
-
-app.use(session({
-  secret: 'keyboard cat',
-  resave: false,
-  saveUninitialized: true,
-  cookie: {secure : false}
-}))
