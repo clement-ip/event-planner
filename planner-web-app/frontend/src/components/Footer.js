9709f66ea1bd5f77ee9ca33513eca67500ab9615
eclam <lameric1994@gmail.com>
2020-12-07 19:27:23 -0800

    update footer

--- planner-web-app/frontend/src/components/Footer.js
+++ planner-web-app/frontend/src/components/Footer.js
@@ -8,14 +8,14 @@ const Footer = () => {
             <div className="container">
                 <div className="columns">
                     <div className="column is-one-third">
-                        <p className="is-size-3"><b>App Name</b></p>
-                        <p className="has-text-grey">App Motto</p>
+                        <p className="is-size-3"><b>Timinar</b></p>
+                        <p className="has-text-grey">Connecting People in Time of Need.</p>
                     </div>
                     <div className="column is-2 mt-2">
                         <p><strong>Explore</strong></p>
                         <p className="is-size-7"><a href='/' className="has-text-grey">Home</a></p>
                         <p className="is-size-7"><a href={`/viewProfile/${user.userID}`} className="has-text-grey">Profile</a></p>
-                        <p className="is-size-7"><a className="has-text-grey">About</a></p>
+                        <p className="is-size-7"><a href='/about' className="has-text-grey">About</a></p>
                     </div>
                     <div className="column is-3 mt-2">
                         <p><strong>Connect</strong></p>
@@ -24,7 +24,7 @@ const Footer = () => {
                                 <i className="fab fa-github-alt"></i>
                             </span>
                             <a href="https://csil-git1.cs.surrey.sfu.ca/kci1/470-planner-web-app" className="has-text-grey">
-                                Github
+                                Gitlab
                             </a>
                         </p>
                     </div>
